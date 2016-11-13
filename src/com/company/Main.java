ae37cec95626e441530a8aa019dfde074fe46777
Анна <anechkamorozova11@rambler.ru>
2016-11-13 13:27:34 +0300

    12

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a;
+        a=1;
+        for(int i=0;i<3;i++)
+        {
+            a=a+1;
+            System.out.println(a);
+        }
+    }
+}
